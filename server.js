d750393041d1ddf8888340df6c16a330ea2e2d07
baldo-vela <baldo.vela+github@gmail.com>
2021-05-18 18:03:33 -0500

    Implements Node server and empty pages

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const path = require("path")
+
+const app = express();
+
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")))
+
+app.get("/*", 
+    (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+});
+// takes incoming requests and responds with the index.html
+
+app.listen(process.env.PORT || 3000, () => console.log("Server Running..."));
