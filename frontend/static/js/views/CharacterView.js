424843e5fff2f5f3f00128b32c7720846502d46c
baldo-vela <baldo.vela+github@gmail.com>
2021-05-24 15:46:18 -0500

    Ties CharacterView and CSS together

--- frontend/static/js/views/CharacterView.js
+++ frontend/static/js/views/CharacterView.js
@@ -3,14 +3,14 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
     constructor() {
         super();
-        this.postId = params.id;
+        this.characterId = params.id;
         this.setTitle("Viewing Post");
     }
 
     async getHtml() {
         return `
             <h1>Post</h1>
-            <p>You are viewing post #${this.postId}.</p>
+            <p>You are viewing post #${this.characterId}.</p>
         `;
     }
 }
