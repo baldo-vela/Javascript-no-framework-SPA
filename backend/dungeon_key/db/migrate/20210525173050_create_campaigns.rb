33c933171cae1ef4602a23dccd3055761f52a512
baldo-vela <baldo.vela+github@gmail.com>
2021-05-27 17:13:43 -0500

    Removes excess commas, builds schema

--- backend/dungeon_key/db/migrate/20210525173050_create_campaigns.rb
+++ backend/dungeon_key/db/migrate/20210525173050_create_campaigns.rb
@@ -1,9 +1,9 @@
 class CreateCampaigns < ActiveRecord::Migration[6.0]
   def change
     create_table :campaigns do |t|
-      t.string :name,
+      t.string :name
       t.text :description
-      
+
       t.timestamps
     end
   end
---
+++ backend/dungeon_key/db/schema.rb
@@ -0,0 +1,32 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_05_27_215811) do
+
+  create_table "campaigns", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "npcs", force: :cascade do |t|
+    t.string "name"
+    t.text "notes"
+    t.integer "campaign_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["campaign_id"], name: "index_npcs_on_campaign_id"
+  end
+
+  add_foreign_key "npcs", "campaigns"
+end
