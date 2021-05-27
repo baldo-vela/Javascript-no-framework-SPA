6147559f687b815fbefcb1fd03e7ced3d118c488
baldo-vela <baldo.vela+github@gmail.com>
2021-05-27 17:08:29 -0500

    Adds associative foreign key to migration

--- backend/dungeon_key/db/migrate/20210527215811_create_npcs.rb
+++ backend/dungeon_key/db/migrate/20210527215811_create_npcs.rb
@@ -3,6 +3,7 @@ class CreateNpcs < ActiveRecord::Migration[6.0]
     create_table :npcs do |t|
       t.string :name
       t.text :notes
+      t.belongs_to :campaign, null: false, foreign_key: true
 
       t.timestamps
     end
