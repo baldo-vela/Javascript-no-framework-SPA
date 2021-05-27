22195c3c6f3995fdc5b72d3fe1d07f5f4e53e089
baldo-vela <baldo.vela+github@gmail.com>
2021-05-27 14:53:06 -0500

    Adds annotation to Game view

--- frontend/static/js/views/Games.js
+++ frontend/static/js/views/Games.js
@@ -1,5 +1,7 @@
 import AbstractView from "./AbstractView.js";
 //Okay lets define a game system
+//Game has a Name, and a Descrip
+//A Game has many NPC's
 export default class extends AbstractView {
     constructor(params) {
         // Need the super constructor to override the parent class
