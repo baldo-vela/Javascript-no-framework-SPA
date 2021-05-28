1ab90a97c7938326c3c7e82f738a411a9919db64
baldo-vela <baldo.vela+github@gmail.com>
2021-05-28 12:29:15 -0500

    Populates seed file, alters css load order

--- backend/dungeon_key/db/seeds.rb
+++ backend/dungeon_key/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+test_campaign = Campaign.create(name: "Test")
+backup_campaign = Campaign.create(name: "Alternate", description: "This is a test of the description value.")
+
+test_campaign.npcs.build(
+    name: "Joe Dirt",
+    notes: "The number of David Spade movies far excedes the number of good David Spade movies."
+)
+
+backup_campaign.npcs.build(
+    name: "Steve the Pirate",
+    notes: "'Yarr, I be the dread pirate Steve!'"
+)
