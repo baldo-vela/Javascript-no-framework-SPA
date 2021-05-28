da51f8f33bd0015810da7dcbed41225a1ab222ef
baldo-vela <baldo.vela+github@gmail.com>
2021-05-28 11:57:26 -0500

    Further refines dashboard

--- frontend/static/js/views/Dashboard.js
+++ frontend/static/js/views/Dashboard.js
@@ -4,12 +4,12 @@ export default class extends AbstractView {
     constructor(params) {
         // Need the super constructor to override the parent class
         super(params);
-        this.setTitle("Dashboard");
+        this.setTitle("DK:Dashboard");
     }
 
     async getHtml() {
         return `
-        <h1>Lorem Ipsum</h1>
+        <h1>Dungeon Key</h1>
         <h4>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</h4>
         <h5>"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</h5>
         
