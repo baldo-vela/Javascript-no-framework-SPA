d908cab848b613cf68c2c53d8d6e00814c49fb04
baldo-vela <baldo.vela+github@gmail.com>
2021-05-27 19:27:51 -0500

    Updates routes and builds out api controller

--- backend/dungeon_key/config/routes.rb
+++ backend/dungeon_key/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :npcs
-  resources :campaigns
+  namespace :api do
+    namespace :v1 do
+      resources :npcs
+      resources :campaigns
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
