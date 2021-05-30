e68ca14bcc648e6040402149804a8001417c19c3
baldo-vela <baldo.vela+github@gmail.com>
2021-05-29 21:46:34 -0500

    Initializes npc class

---
+++ frontend/static/js/components/npcs.js
@@ -0,0 +1,12 @@
+class Npc {
+    static all = []
+
+    constructor(params){
+        this.id = params.id
+        this.name = params.name
+        this.campaignId = params.campaign_id
+
+        Npc.all.push(this)
+    }
+    
+}
